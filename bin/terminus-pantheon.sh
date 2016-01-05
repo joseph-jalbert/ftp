88deb3871d8641dfc168c8b51edcca91c3284c3a
Mat Gargano <mgargano@forthepeople.com>
2016-01-05 15:36:12 -0500

    when deploying turn on SFTP mode before pushing up wp-redis plugin, then turn git mode back on

--- bin/terminus-pantheon.sh
+++ bin/terminus-pantheon.sh
@@ -7,7 +7,9 @@ if hash terminus 2>/dev/null; then
     if [ $1 = "master" ]; then
 
         #only install it do not activate it it https://pantheon.io/docs/articles/wordpress/installing-redis-on-wordpress/#install-drop-in-plugin
+        terminus site set-connection-mode --mode=sftp --site=$PANTHEON_SITE_NAME
         terminus wp plugin install wp-redis --site=$PANTHEON_SITE_NAME --env=dev
+        terminus site set-connection-mode --mode=git --site=$PANTHEON_SITE_NAME
         terminus wp rewrite flush  --site=$PANTHEON_SITE_NAME --env=live
         terminus site deploy --site=$PANTHEON_SITE_NAME --env=live --note="Updating"
         terminus wp plugin deactivate wp-redis --site=$PANTHEON_SITE_NAME --env=live

48016ac7816d03fb06d2c7e480d58cedfb4cd2ae
Mat Gargano <mgargano@forthepeople.com>
2016-01-05 14:47:54 -0500

    update terminus-pantheon.sh to install plugin on development

--- bin/terminus-pantheon.sh
+++ bin/terminus-pantheon.sh
@@ -5,11 +5,12 @@ if hash terminus 2>/dev/null; then
     terminus auth login $PANTHEON_USER --password=$PANTHEON_PASSWORD
 
     if [ $1 = "master" ]; then
-        terminus site deploy --site=$PANTHEON_SITE_NAME --env=live --note="Updating"
+
         #only install it do not activate it it https://pantheon.io/docs/articles/wordpress/installing-redis-on-wordpress/#install-drop-in-plugin
-        terminus wp plugin install wp-redis --site=$PANTHEON_SITE_NAME --env=live
-        terminus wp plugin deactivate wp-redis --site=$PANTHEON_SITE_NAME --env=live
+        terminus wp plugin install wp-redis --site=$PANTHEON_SITE_NAME --env=dev
         terminus wp rewrite flush  --site=$PANTHEON_SITE_NAME --env=live
+        terminus site deploy --site=$PANTHEON_SITE_NAME --env=live --note="Updating"
+        terminus wp plugin deactivate wp-redis --site=$PANTHEON_SITE_NAME --env=live
     else
         terminus site deploy --site=$PANTHEON_SITE_NAME --env=test --note="Updating"
     fi

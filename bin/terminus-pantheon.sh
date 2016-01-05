de8c0f92c99694a251bb6a1e5901e21896d13000
Mat Gargano <mgargano@forthepeople.com>
2016-01-05 11:01:24 -0500

    update terminus-pantheon.sh to be more specific on wp-redis

--- bin/terminus-pantheon.sh
+++ bin/terminus-pantheon.sh
@@ -6,8 +6,9 @@ if hash terminus 2>/dev/null; then
 
     if [ $1 = "master" ]; then
         terminus site deploy --site=$PANTHEON_SITE_NAME --env=live --note="Updating"
+        #only install it do not activate it it https://pantheon.io/docs/articles/wordpress/installing-redis-on-wordpress/#install-drop-in-plugin
         terminus wp plugin install wp-redis --site=$PANTHEON_SITE_NAME --env=live
-        terminus wp plugin activate wp-redis --site=$PANTHEON_SITE_NAME --env=live
+        terminus wp plugin deactivate wp-redis --site=$PANTHEON_SITE_NAME --env=live
         terminus wp rewrite flush  --site=$PANTHEON_SITE_NAME --env=live
     else
         terminus site deploy --site=$PANTHEON_SITE_NAME --env=test --note="Updating"
@@ -16,18 +17,4 @@ if hash terminus 2>/dev/null; then
 else
     echo 'Please install terminus see https://github.com/pantheon-systems/cli'
 fi
-
-
-
-
-
-#if [ -n "${RUN_NIGHTLY_BUILD}" ]; then
-
-   #terminus wp migratedb push https://dev-abogados.pantheon.io R9pP7iBxg2XqWJFNIzK1GUfpC9z7B0f0aFBBNSzg --site=$PANTHEON_SITE_NAME --env=live --media=compare-and-remove
-   #terminus wp migratedb push https://test-abogados.pantheon.io TnQq+huu2LXUTKF3JGQcY9N4DJNo5/+TB6sTzZq2 --site=$PANTHEON_SITE_NAME --env=live --media=compare-and-remove
-   #terminus wp migratedb push https://abogadosdr.wpengine.com YGgf9+4YRIVPP/4/WSXb2pYOD2vH9psrJb4ohCk9 --site=$PANTHEON_SITE_NAME --env=live --media=compare-and-remove
-   #terminus wp migratedb push https://abogadosdr.staging.wpengine.com YB/hrl+1Nj4buEEaDhHq6SzD5R4k6oVxJLva6kcK --site=$PANTHEON_SITE_NAME --env=live --media=compare-and-remove
-
-
-#fi
 

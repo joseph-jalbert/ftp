c3df23045e38686323637779d9e7f7ef4f95d6a7
Mat Gargano <mgargano@forthepeople.com>
2016-01-12 16:31:17 -0500

    update news-taxonomy

--- web/app/themes/forthepeople/inc/news-taxonomy.php
+++ web/app/themes/forthepeople/inc/news-taxonomy.php
@@ -3,25 +3,37 @@
 
 class Location_Taxonomy {
 
-	const TAXONOMY = 'office_location';
-	const TERM_OPTION_NAME = 'office_location_locations';
-	const SLUG = 'news';
+	const LOCATION_TAXONOMY = 'office_location';
+	const CATEGORY_TAXONOMY = 'location_category';
+	const LOCATION_TERM_OPTION_NAME = 'office_location_locations';
+	const LOCATION_POST_ID_OPTION_NAME = 'office_location_locations';
+	const LOCATION_TAXONOMY_SLUG = 'news';
+	const CATEGORY_TAXONOMY_SLUG = 'category';
+	const POST_TYPE = 'post';
+
 
 	public static function init() {
 		self::attach_hooks();
 	}
 
 	public static function attach_hooks() {
-		add_action( 'init', array( __CLASS__, 'register_taxonomy' ) );
+		add_action( 'init', array( __CLASS__, 'register_taxonomies' ) );
 		add_action( 'init', array( __CLASS__, 'taxonomy_rewrite_rule' ) );
 
-		add_action( 'create_term', array( __CLASS__, 'flush_option' ), 10, 3 );
-		add_action( 'edit_term', array( __CLASS__, 'flush_option' ), 10, 3 );
-		add_action( 'delete_term', array( __CLASS__, 'flush_option' ), 10, 3 );
-		add_filter( 'term_link', array( __CLASS__, 'filter_term_link' ), 10, 3 );
+		add_action( 'create_term', array( __CLASS__, 'flush_location_option' ), 10, 3 );
+		add_action( 'edit_term', array( __CLASS__, 'flush_location_option' ), 10, 3 );
+		add_action( 'delete_term', array( __CLASS__, 'flush_location_option' ), 10, 3 );
+		add_filter( 'term_link', array( __CLASS__, 'filter_location_term_link' ), 10, 3 );
+		add_action( 'save_post', array( __CLASS__, 'flush_location_post_id_option' ), 10, 3 );
+		add_action( 'pre_get_posts', array( __CLASS__, 'remove_location_posts_from_main_blog' ) );
 	}
 
-	public static function register_taxonomy() {
+	public static function register_taxonomies() {
+		self::register_location_taxonomy();
+		self::register_category_taxonomy();
+	}
+
+	public static function register_location_taxonomy() {
 		$labels = array(
 			'name'              => _x( 'Location', 'taxonomy general name' ),
 			'singular_name'     => _x( 'Location', 'taxonomy singular name' ),
@@ -43,69 +55,183 @@ class Location_Taxonomy {
 			'show_admin_column' => true,
 			'query_var'         => true,
 			'rewrite'           => array(
-				'slug'       => self::SLUG,
+				'slug'       => self::LOCATION_TAXONOMY_SLUG,
 				'with_front' => true
 			),
 		);
 
-		register_taxonomy( self::TAXONOMY, array( 'post' ), $args );
+		register_taxonomy( self::LOCATION_TAXONOMY, array( self::POST_TYPE ), $args );
+	}
+
+	public static function register_category_taxonomy() {
+		$labels = array(
+			'name'              => _x( 'Local Categories', 'taxonomy general name' ),
+			'singular_name'     => _x( 'Local Category', 'taxonomy singular name' ),
+			'search_items'      => __( 'Search Local Category' ),
+			'all_items'         => __( 'All Local Category' ),
+			'parent_item'       => __( 'Parent Local Category' ),
+			'parent_item_colon' => __( 'Parent Local Category:' ),
+			'edit_item'         => __( 'Edit Local Category' ),
+			'update_item'       => __( 'Update Local Category' ),
+			'add_new_item'      => __( 'Add New Local Category' ),
+			'new_item_name'     => __( 'New Local Category Name' ),
+			'menu_name'         => __( 'Local Category' ),
+		);
+
+		$args = array(
+			'hierarchical'      => true,
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array(
+				'slug'       => self::CATEGORY_TAXONOMY_SLUG,
+				'with_front' => true
+			),
+		);
+
+		register_taxonomy( self::CATEGORY_TAXONOMY, array( self::POST_TYPE ), $args );
 	}
 
 
 	public static function taxonomy_rewrite_rule() {
 
-		$terms = self::get_terms();
+		$terms = self::get_location_terms();
+		if ( ! is_array( $terms ) ) {
+			return;
+		}
 		foreach ( $terms as $slug ) {
 			add_rewrite_rule( '^' . $slug . '/news/?', 'index.php?office_location=' . $slug, 'top' );
 		}
 
 	}
 
-	public static function get_terms() {
+	public static function get_location_terms() {
 
-		$term_option_names = get_option( self::TERM_OPTION_NAME );
-		if ( ! $term_option_names ) {
-			$term_option_names = self::build_term_option();
+		$location_term_option_names = get_option( self::LOCATION_TERM_OPTION_NAME );
+		if ( ! $location_term_option_names ) {
+			$location_term_option_names = self::build_location_term_option();
 		}
 
-		return $term_option_names;
+		return $location_term_option_names;
 
 	}
 
-	public static function build_term_option() {
+	public static function build_location_term_option() {
 
-		self::kill_term_option();
-		$terms = get_terms( self::TAXONOMY, array( 'hide_empty' => false ) );
+		self::kill_location_term_option();
+		$terms = get_terms( self::LOCATION_TAXONOMY, array( 'hide_empty' => false ) );
 		$slugs = array();
 		foreach ( $terms as $term ) {
 			$slugs[] = $term->slug;
 		}
 
-		$term_option_names = update_option( self::TERM_OPTION_NAME, $slugs );
+		$location_term_option_names = update_option( self::LOCATION_TERM_OPTION_NAME, $slugs );
 
-		return $term_option_names;
+		return $location_term_option_names;
 	}
 
-	public static function kill_term_option() {
+	public static function kill_location_term_option() {
 
-		delete_option( self::TERM_OPTION_NAME );
+		delete_option( self::LOCATION_TERM_OPTION_NAME );
 
 	}
 
-	public static function flush_option( $term_id, $tt_id, $taxonomy ) {
-		if ( self::TAXONOMY === $taxonomy ) {
-			self::build_term_option();
+	public static function flush_location_option( $_, $_, $taxonomy ) {
+		if ( self::LOCATION_TAXONOMY === $taxonomy ) {
+			self::build_location_term_option();
 			flush_rewrite_rules();
 		}
 
 	}
 
-	public static function filter_term_link( $term_link, $term, $taxonomy ) {
+	public static function get_location_post_ids() {
 
-		if ( self::TAXONOMY === $taxonomy ) {
+		$location_post_ids_option_names = get_option( self::LOCATION_POST_ID_OPTION_NAME );
+		if ( ! $location_post_ids_option_names ) {
+			$location_post_ids_option_names = self::build_location_post_id_option();
+		}
 
-			$term_link = str_replace( array( '/' . self::SLUG . '/', '/' . $term->slug . '/' ), array( '/%SLUG%/', '/%TERM%/' ), $term_link );
-			$term_link = str_replace( array( '%SLUG%', '%TERM%' ), array($term->slug, self::SLUG), $term_link );
+		return $location_post_ids_option_names;
+
+	}
+
+	public static function build_location_post_id_option() {
+
+		self::kill_location_post_id_option();
+		$post_ids = array();
+		$term_ids = get_terms(
+			self::LOCATION_TAXONOMY,
+			array(
+				'fields' => 'ids'
+			)
+		);
+		if ( $term_ids
+		     && ! is_wp_error( $term_ids )
+		) {
+			$args  = array(
+				'tax_query' => array(
+					array(
+						'taxonomy' => self::LOCATION_TAXONOMY,
+						'terms'    => $term_ids,
+					)
+				),
+			);
+			$posts = new WP_Query( $args );
+			if ( $posts->have_posts() ) {
+				while ( $posts->have_posts() ) {
+					$posts->the_post();
+					$post_ids[] = get_the_ID();
+
+
+				}
+				wp_reset_postdata();
+
+			}
+
+		}
+		$location_post_ids_option_names = update_option( self::LOCATION_POST_ID_OPTION_NAME, $post_ids );
+
+		return $location_post_ids_option_names;
+
+
+	}
+
+	public static function kill_location_post_id_option() {
+
+		delete_option( self::LOCATION_POST_ID_OPTION_NAME );
+
+	}
+
+	public static function flush_location_post_id_option( $post_id, $post, $update ) {
+
+		if ( $post->post_type === self::POST_TYPE && ( $update === true || wp_get_post_terms( $post_id, self::LOCATION_TAXONOMY ) ) ) {
+			self::build_location_post_id_option();
+		}
+
+	}
+
+	public static function remove_location_posts_from_main_blog( WP_Query $query ) {
+
+		if ( ! is_admin() && $query->is_main_query() && $query->is_home() ) {
+			$post_not_in_extant = $query->get( 'post__not_in' );
+			$query->set( 'post__not_in', array_merge( (array) $post_not_in_extant, (array) self::get_location_post_ids() ) );
+		}
+
+	}
+
+	public static function filter_location_term_link( $term_link, $term, $taxonomy ) {
+
+		if ( self::LOCATION_TAXONOMY === $taxonomy ) {
+
+			$term_link = str_replace( array( '/' . self::LOCATION_TAXONOMY_SLUG . '/', '/' . $term->slug . '/' ), array(
+				'/%SLUG%/',
+				'/%TERM%/'
+			), $term_link );
+			$term_link = str_replace( array( '%SLUG%', '%TERM%' ), array(
+				$term->slug,
+				self::LOCATION_TAXONOMY_SLUG
+			), $term_link );
 
 		}
 

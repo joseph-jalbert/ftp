dcb5e25b1cc07d5062dd96dcb8a134bed7243b59
Mat Gargano <mgargano@forthepeople.com>
2016-01-19 09:30:42 -0500

    add block robots plugin

---
+++ web/app/plugins/block-robots/block-robots.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Block Robots
+Plugin URI: http://www.forthepeople.com/
+Description: This plugin blocks robots.txt if the domain is not www.[site].com, [site].com or if the domain has a tld of .dev
+Version: 0.1
+Author: Mat Gargano
+Author URI: http://www.forthepeople.com/
+License: GPL
+Copyright: Morgan and Morgan
+*/
+
+require_once __DIR__ . '/inc/block-robots.php';

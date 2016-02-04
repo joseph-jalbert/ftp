6f9e88d00a39f5aebe42c2d4ad162d5c369387fe
Circle CI <circleci@forthepeople.com>
2016-02-04 13:55:27 -0500

    set IE=Edge

--- web/app/themes/forthepeople/header.php
+++ web/app/themes/forthepeople/header.php
@@ -9,7 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>><head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.png">
 <link rel="apple-touch-startup-image" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/startup.png">

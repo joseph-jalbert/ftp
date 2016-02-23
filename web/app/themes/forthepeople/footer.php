0d0297e191c0e85769e2bd69d8e247ae20902d6f
Mat Gargano <mgargano@forthepeople.com>
2016-02-23 10:33:30 -0500

    remove hubspot js inclusion from footer

--- web/app/themes/forthepeople/footer.php
+++ web/app/themes/forthepeople/footer.php
@@ -55,10 +55,6 @@
           <div class="row-fluid">
 			<div class="consumer-alerts-form" id="consumer-alerts-form">
 
-				<!--[if lte IE 8]>
-				<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
-				<![endif]-->
-				<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
 				<script>
 					hbspt.forms.create({
 						portalId: '1841598',

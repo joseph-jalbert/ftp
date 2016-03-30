0d66c04bba247547e8dcd30e1d56394af639d206
Mat Gargano <mgargano@forthepeople.com>
2016-03-30 19:13:02 -0400

    do not migrate db overnight

--- bin/controller.sh
+++ bin/controller.sh
@@ -10,7 +10,7 @@ if [ -n "${RUN_NIGHTLY_BUILD}" ]; then
         terminus wp --site=$PANTHEON_SITE_NAME --env=dev mm add_compatibility_plugin
         terminus wp --site=$PANTHEON_SITE_NAME --env=dev mm set_blacklists
         terminus wp plugin activate wp-migrate-db-pro wp-migrate-db-pro-media-files wp-migrate-db-pro-cli --site=$PANTHEON_SITE_NAME --env=dev
-        terminus wp migratedb push $DESTINATION_WPDBMIGRATE_PRO_STRING --media=compare-and-remove --site=$PANTHEON_SITE_NAME --env=dev
+        #terminus wp migratedb push $DESTINATION_WPDBMIGRATE_PRO_STRING --media=compare-and-remove --site=$PANTHEON_SITE_NAME --env=dev
         terminus site set-connection-mode --mode=git --site=$PANTHEON_SITE_NAME
 
     elif [ $1 = "master" ]; then

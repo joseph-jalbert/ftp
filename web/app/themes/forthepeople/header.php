6929d43e1cfa1c3e67b7cc9a22d064a55b37ddd8
Mat Gargano <mgargano@forthepeople.com>
2016-04-20 12:40:42 -0400

    all business littigation lawyer pages use business litigation menu

--- web/app/themes/forthepeople/header.php
+++ web/app/themes/forthepeople/header.php
@@ -81,7 +81,7 @@ if(is_page_template('all-class-actions.php') || is_singular('classactionlawyers'
 $menuslug = 'main-navigation';
 }
 
-if ( is_page_template( 'business-litigation.php' ) || is_child( '6110' ) || $post_type === BTG_News::POST_TYPE || BTG_News::get_query_var_value() ) {
+if ( $post->post_name === 'business-litigation-lawyers' || is_page_template( 'business-litigation.php' ) || is_child( '6110' ) || $post_type === BTG_News::POST_TYPE || BTG_News::get_query_var_value() ) {
 $menuslug = 'business-litigation';
 }
 

56631616947bdfe2e931ffc3d789552c0e41e5a2
Bill Van Pelt <linusx@gmail.com>
2016-05-02 13:38:31 -0400

    Adding caching to the related content calls

--- web/app/themes/forthepeople/inc/widgets/related-content/related-content.php
+++ web/app/themes/forthepeople/inc/widgets/related-content/related-content.php
@@ -4,12 +4,14 @@ class Related_Content extends WP_Widget {
 
 	protected static $text_domain = 'related_content';
 	protected static $ver = '0.1';
+	protected static $transient_key = 'ftp-related-posts-'; // Will stick the post id at the end
 
 	/**
 	 * Initialization method
 	 */
 	public static function init() {
 		add_action( 'widgets_init', create_function( '', 'register_widget( "related_content" );' ) );
+		add_action( 'save_post', array( __CLASS__, 'save_post' ) );
 	}
 
 	/**
@@ -17,12 +19,11 @@ class Related_Content extends WP_Widget {
 	 */
 	public function __construct() {
 		parent::__construct(
-			'related_content', // Base ID
-			'Related Content', // Name
-			array( 'description' => __( 'Related Content', self::$text_domain ), ) // Args
+			'related_content',
+			'Related Content',
+			array( 'description' => __( 'Related Content', self::$text_domain ), )
 		);
 	}
-
 
 	/**
 	 * Front-end display of widget.
@@ -35,8 +36,63 @@ class Related_Content extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		$current_post = get_queried_object();
-		$post_id = $current_post ? $current_post->ID : null;
-		$related_posts = array();
+		$post_id      = $current_post ? $current_post->ID : null;
+
+		if ( ! $post_id ) {
+			return;
+		}
+
+		$related_posts = self::get_related_posts( $post_id );
+		if ( false === $related_posts ) :
+			return;
+		endif;
+
+		echo $before_widget; ?>
+		<div
+			class="execphpwidget">
+			<div
+				class="widgetWrap aside row-leading">
+				<div
+					class="title">
+					<span>Related Content</span>
+				</div>
+				<div
+					class="body related-content">
+					<ul class="related-content"><?php
+						if ( $related_posts->have_posts() ) :
+							while ( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
+								<li>
+								<p class="title"><?php the_title(); ?></p>
+								<p class="content"><?php echo rtrim( substr( get_the_content( null, true ), 0, 150 ) ); ?>
+									...<br/><a
+										href="<?php the_permalink(); ?>"
+										alt="<?php echo the_title(); ?>">Read
+										More</a>
+								</p>
+								</li><?php
+							endwhile;
+						endif; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<?php echo $after_widget;
+	}
+
+	/**
+	 * Get related content based on location_category custom taxonomy
+	 *
+	 * @param $post_id
+	 *
+	 * @return bool|mixed|WP_Query
+	 */
+	public static function get_related_posts( $post_id ) {
+		$transient_key = self::$transient_key . $post_id;
+		$related_posts = get_transient( $transient_key );
+
+		if ( false !== $related_posts ) :
+			return $related_posts;
+		endif;
 
 		$local_category = get_the_terms( $post_id, 'location_category' );
 		if ( ! empty( $local_category ) ) :
@@ -45,85 +101,42 @@ class Related_Content extends WP_Widget {
 				if ( 'location_category' === $category->taxonomy ) :
 					$categories[] = array(
 						'taxonomy' => 'location_category',
-						'field' => 'slug',
-						'terms' => $category->slug
+						'field'    => 'slug',
+						'terms'    => $category->slug
 					);
 				endif;
 			endforeach;
 
 			$args = array(
 				'posts_per_page' => 3,
-				'orderby' => 'rand',
-				'post_type' => 'local_news',
-				'post_status' => 'publish',
-				'post__not_in' => array($post_id),
-				'tax_query' => $categories
+				'orderby'        => 'rand',
+				'post_type'      => 'local_news',
+				'post_status'    => 'publish',
+				'post__not_in'   => array( $post_id ),
+				'tax_query'      => $categories
 			);
 
-			//$related_posts = get_posts( $args );
 			$related_posts = new WP_Query( $args );
 			if ( empty ( $related_posts ) ) :
 				return false;
 			endif;
+
+			set_transient( $transient_key, $related_posts );
+
+			return $related_posts;
 		endif;
 
-		?>
-
-		<?php echo $before_widget; ?>
-			<div class="execphpwidget">
-				<div class="widgetWrap aside row-leading">
-					<div class="title"><span>Related Content</span></div>
-					<div class="body related-content">
-						<ul class="related-content"><?php
-						if ($related_posts->have_posts()) :
-							while ($related_posts->have_posts()) : $related_posts->the_post(); ?>
-								<li>
-									<p class="title"><?php the_title(); ?></p>
-									<p class="content"><?php echo rtrim( substr( get_the_content( null, true ), 0, 150 ) ); ?>...<br/><a href="<?php the_permalink(); ?>" alt="<?php echo the_title(); ?>">Read More</a></p>
-								</li><?php
-							endwhile;
-						endif; ?>
-						</ul>
-					</div>
-				</div>
-			</div>
-		<?php echo $after_widget; ?>
-		<?php
+		return false;
 	}
 
 	/**
-	 * Sanitize widget form values as they are saved.
+	 * Delete the transient for the related content
 	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
+	 * @param $post_id
 	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		return $instance;
-	}
-
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		$defaults = array(
-			'hubspot-form-id'   => null,
-			'hubspot-portal-id' => null,
-			'hubspot-target' => null,
-
-		);
-		$instance = wp_parse_args( (array) $instance, $defaults );
-		?>
-		<div class="related-content-form"></div>
-		<?php
+	public function save_post( $post_id ) {
+		$transient_key = self::$transient_key . $post_id;
+		delete_transient( $transient_key );
 	}
 }
 

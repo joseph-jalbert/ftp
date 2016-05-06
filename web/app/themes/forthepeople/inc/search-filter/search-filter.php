d3421fdb1c218bd7b8458898063cb78ab4256c85
Mat Gargano <mgargano@forthepeople.com>
2016-05-06 14:00:28 -0400

    fix search functionality to handle if a meta query exists

--- web/app/themes/forthepeople/inc/search-filter/search-filter.php
+++ web/app/themes/forthepeople/inc/search-filter/search-filter.php
@@ -21,8 +21,9 @@ class Search_Filter {
 	public static function search_query( $query ) {
 
 		if ( $query->is_search ) {
-			$meta_query = array(
-				'relation'    => 'OR',
+
+			$meta_query[] = $search_meta_query = array(
+				'relation' => 'OR',
 				array(
 					'key'     => self::$exclude_posts_field,
 					'compare' => 'NOT EXISTS'
@@ -33,7 +34,18 @@ class Search_Filter {
 					'compare' => '!='
 				),
 			);
+
+			$existing_meta_query = $query->get( 'meta_query' );
+
+			if ( $existing_meta_query && is_array( $existing_meta_query ) ) {
+				foreach ( $existing_meta_query as $current_meta_query ) {
+					$meta_query[] = $current_meta_query;
+				}
+			}
+
 			$query->set( 'meta_query', $meta_query );
+
+
 		}
 
 		return $query;

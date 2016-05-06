e6429b11c5c64b43d5830ea3dce5f06bc45b2651
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2016-05-06 13:24:04 -0400

    Link 'Contact Us' button to location specific page

--- web/app/themes/forthepeople/assets/js/scripts/global.js
+++ web/app/themes/forthepeople/assets/js/scripts/global.js
@@ -162,7 +162,10 @@ checkStickyNav = function(){
   if( $('div.modal') ){ $(document.body).append( $('div.modal').detach() ); }
 }); //end doc rdy
 
-function contactPush(){window.location ='/free-case-evaluation';}
+function contactPush(){
+    var old_url = window.location.href;
+    window.location = old_url + 'free-case-evaluation';
+}
 function getRandomInt (min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;};
 function trackEventGA(Category, Action, Label, Value) {
     "use strict";

dd428a9915c54ed1533aeb25970c8e14f2ea1875
Mat Gargano <mgargano@forthepeople.com>
2016-05-16 12:51:42 -0400

    add lucky orange script

--- web/app/themes/forthepeople/inc/actions.php
+++ web/app/themes/forthepeople/inc/actions.php
@@ -11,9 +11,8 @@ class Actions {
 	public static function attach_hooks() {
 
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue' ) );
-		// only adding a filter here because it pertains to an action
 		add_action( 'script_loader_tag', array( __CLASS__, 'enqueue_additions' ), 10, 2 );
-
+		add_action( 'wp_footer', array( __CLASS__, 'footer_scripts' ) );
 
 	}
 
@@ -31,6 +30,20 @@ class Actions {
 		wp_enqueue_script( 'hubspot-ie8-script', '//js.hsforms.net/forms/v2-legacy.js' );
 		wp_enqueue_script( 'hubspot-script', '//js.hsforms.net/forms/v2.js' );
 
+	}
+
+	public static function footer_scripts(){
+		?>
+		<script type='text/javascript'>
+			window.__lo_site_id = 57538;
+
+			(function() {
+				var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+				wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+			})();
+		</script>
+		<?php
 	}
 
 

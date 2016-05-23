5131a9bb52868c3b8a77f84909dc5584c7069902
Bill Van Pelt <linusx@gmail.com>
2016-05-23 14:09:37 -0400

    Forgot the link

--- web/app/themes/forthepeople/template-parts/content-search.php
+++ web/app/themes/forthepeople/template-parts/content-search.php
@@ -7,7 +7,6 @@
  * @package ForThePeople
  */
 
-/** Get the Yoast SEO title. If there is non, then show default title */
 $title = get_post_meta($post->ID, '_yoast_wpseo_title', true);
 if ( empty ( $title ) ) :
 	$title = get_the_title( $post->ID );
@@ -16,7 +15,8 @@ endif;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php echo $title; ?></h1>
+		<h1 class="entry-title"><a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark"><?php echo $title; ?></a></h1>
+
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php forthepeople_posted_on(); ?>

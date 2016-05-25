3aeb350b0d3104610218f9539ec7c5c6f1d1a9a0
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2016-05-25 12:20:25 -0400

    Enable voiceover of dropdown menu elements once list is expanded

--- web/app/themes/forthepeople/js/aria.js
+++ web/app/themes/forthepeople/js/aria.js
@@ -1,11 +1,17 @@
 ( function( $ ) {
     // bind a click event to the 'skip' link
     $(".skip-link").click(function(event){
-        
         // strip the leading hash and declare
         // the content we're skipping to
         var skipTo="#"+this.href.split('#')[1];
 
         $(skipTo).focus(); // focus on the content container
     });
+
+    //set aria-visible to true when focused inside a dropdown menu
+
+    $(".dropdown-menu > li").focus(function(){
+        console.log('SUCCESS');
+        $(this).parent().attr('aria-hidden', 'false');
+    })
 }) ( jQuery );

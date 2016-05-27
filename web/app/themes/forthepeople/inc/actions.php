be49c86dbda296d74d2f8d218341be157264be96
Mat Gargano <mgargano@forthepeople.com>
2016-05-27 11:26:35 -0400

    add pinterest verify (THEY DONT DO DOMAIN VERIFICTION UGH)

--- web/app/themes/forthepeople/inc/actions.php
+++ web/app/themes/forthepeople/inc/actions.php
@@ -13,9 +13,13 @@ class Actions {
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue' ) );
 		add_action( 'script_loader_tag', array( __CLASS__, 'enqueue_additions' ), 10, 2 );
 		add_action( 'wp_footer', array( __CLASS__, 'footer_scripts' ) );
+		add_action( 'wp_head', array( __CLASS__, 'meta_fields' ) );
 
 	}
 
+	public static function meta_fields(){
+		?><meta name="p:domain_verify" content="9bef95dda85621e5f627bfddb6b3c0be"/><?php
+	}
 	public static function enqueue_additions( $tag, $handle ) {
 
 		if ( 'hubspot-ie8-script' === $handle ) {

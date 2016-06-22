d053f8e84d7933f3fc0fe914dc285082963d5035
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2016-06-22 16:58:43 -0400

    Move ShareThis links from sidebar to below content

--- web/app/themes/forthepeople/single.php
+++ web/app/themes/forthepeople/single.php
@@ -32,7 +32,14 @@ get_header(); ?>
             
 
 		<?php endwhile; // end of the loop. ?>
-
+        <?php if (is_single() && ('local_news' === get_post_type() || 'btg_news' === get_post_type()) ) { ?>
+            ​<div class="socialmediawidget horizontal widgetWrap aside clearfix">
+                <span class='st_plusone_vcount' displayText='Google +1'></span>
+                <span class='st_facebook_vcount' displayText='Facebook'></span>
+                <span class='st_twitter_vcount' displayText='Tweet'></span>
+                <span class='st_email_vcount' displayText='Email'></span>
+            </div>
+        <?php }; ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <div id="col2" class="span4">

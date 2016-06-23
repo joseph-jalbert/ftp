8293ed59d1c4040373e7569f0bcd9099fc5652d2
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2016-06-23 12:53:16 -0400

    FTP-125 Add Social Profiles Markup

--- web/app/themes/forthepeople/inc/actions.php
+++ web/app/themes/forthepeople/inc/actions.php
@@ -14,12 +14,28 @@ class Actions {
 		add_action( 'script_loader_tag', array( __CLASS__, 'enqueue_additions' ), 10, 2 );
 		add_action( 'wp_footer', array( __CLASS__, 'footer_scripts' ) );
 		add_action( 'wp_head', array( __CLASS__, 'meta_fields' ) );
+        add_action( 'wp_head', array( __CLASS__, 'social_profiles' ) );
 
 	}
 
 	public static function meta_fields(){
 		?><meta name="p:domain_verify" content="9bef95dda85621e5f627bfddb6b3c0be"/><?php
 	}
+
+    public static function social_profiles() {
+        if( is_page('Home') ) {
+            echo '<script type="application/ld+json">
+                    { "@context" : "http://schema.org",
+                      "@type" : "Organization",
+                      "name" : "Morgan & Morgan",
+                      "url" : "https://www.forthepeople.com",
+                      "sameAs" : [ "https://www.facebook.com/MMForthePeople",
+                        "https://twitter.com/forthepeople",
+                        "https://www.youtube.com/user/mmforthepeople"] 
+                    }
+                    </script>';
+        }
+    }
 	public static function enqueue_additions( $tag, $handle ) {
 
 		if ( 'hubspot-ie8-script' === $handle ) {

0ba340993e334be69044188b19a08d1f2ce79404
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2016-06-28 11:24:11 -0400

    Fix the desktop dropdown menu in Chrome on touch enabled devices

--- web/app/themes/forthepeople/js/aria.js
+++ web/app/themes/forthepeople/js/aria.js
@@ -8,6 +8,16 @@
         $(skipTo).focus(); // focus on the content container
     });
 
+    //add Hover event to dropdown as workaround for Chrome touchbook bug
+
+    var $top_nav = $("li.dropdown");
+    $top_nav.hover(function() {
+        $(this).addClass('open');
+    });
+    $top_nav.mouseout(function() {
+        $(this).removeClass('open');
+    });
+
     //add class 'expanded' when dropdown receives keyboard focus, for screen-readers where mouse pointer does automatically follow keyboard focus
     var $nav_section = $("li.dropdown > a");
     var $dropdown_link = $("ul.dropdown-menu > li > a");
